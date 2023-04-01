3039660357537c5740ae10894c312e3dec414d8a
darabiuz <darayuxin@qq.com>
2023-04-01 15:54:45 +0800

    feat: 项目初始化

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Home from "./routes/home"
+
+function App() {
+
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/routes/home/index.tsx
@@ -0,0 +1,8 @@
+/** 系统主页面 */
+// import { Button } from 'antd'
+const Home = function () {
+    return <>
+    </>
+
+}
+export default Home
